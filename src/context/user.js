0e43a6bd5e4ed7ab1759111b13cf5d9460dd8eb3
mmwandera <mmwandera74@gmail.com>
2023-12-11 16:08:26 +0300

    completed code along

---
+++ src/context/user.js
@@ -0,0 +1,33 @@
+// src/context/user.js
+import React, { useState } from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+// create a provider component
+// function UserProvider({ children }) {
+//   // the value prop of the provider will be our context data
+//   // this value will be available to child components of this provider
+//   return <UserContext.Provider value={null}>{children}</UserContext.Provider>;
+// }
+
+function UserProvider({ children }) {
+  const currentUser = {
+    name: "Duane",
+    interests: ["Coding", "Biking", "Words ending in 'ing'"],
+  };
+  return (
+    <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>
+  );
+}
+
+// function UserProvider({ children }) {
+//   const [user, setUser] = useState(null);
+//   return (
+//     <UserContext.Provider value={{ user, setUser }}>
+//       {children}
+//     </UserContext.Provider>
+//   );
+// }
+
+export { UserContext, UserProvider };
